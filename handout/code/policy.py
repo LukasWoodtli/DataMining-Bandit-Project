4768dd36d6b3a6ef0306bbd3866ac0c160ee9c19
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-12-18 17:46:35 +0100

    Final version of code first version of report

--- handout/code/policy.py
+++ handout/code/policy.py
@@ -1,8 +1,7 @@
 import numpy.random
 import numpy as np
 from numpy.linalg import inv
-ALPHA = 0.5
-
+ALPHA = 0.26
 DIMENSION = 6
 
 
@@ -31,31 +30,38 @@ BestChoice = None
 z_t = None
 
 def set_articles(articles):
-    global AllArticles
-    for x in articles:
-        features = articles[x]
-        article = Article(art_features=features)
-        AllArticles[x] = article
+    # global AllArticles
+    # for x in articles:
+    #     features = articles[x]
+    #     article = Article(art_features=features)
+    #     AllArticles[x] = article
+    pass
 
 def update(reward):
     global AllArticles
-    AllArticles[BestChoice].M = AllArticles[BestChoice].M + np.dot(z_t, z_t)
+    if reward == -1:
+        return
+
+    AllArticles[BestChoice].M = AllArticles[BestChoice].M + np.outer(z_t, z_t)
     AllArticles[BestChoice].M_inv = inv(AllArticles[BestChoice].M)
     AllArticles[BestChoice].b = AllArticles[BestChoice].b + np.multiply(z_t, reward)
+    AllArticles[BestChoice].w = np.dot(AllArticles[BestChoice].M_inv, AllArticles[BestChoice].b)
+    
     
 def reccomend(time, user_features, articles):
     global BestChoice, z_t, AllArticles
-    MaxUCB = 0   # int.inf
+    MaxUCB = 0   # -int.inf
     z_t = np.array(user_features)
     
     for art in articles:
         if not art in AllArticles:
             AllArticles[art] = Article()
         
-        # alles was nicht von y abhaengt vorher berechnen
-        AllArticles[art].w = np.inner(AllArticles[art].M_inv, AllArticles[art].b)
+        # Everything moved to update, where possible
         UCB = np.dot(AllArticles[art].w, z_t) + ALPHA * np.sqrt(np.dot(np.dot(z_t, AllArticles[art].M_inv), z_t))
+        
         if UCB > MaxUCB:
             MaxUCB = UCB
             BestChoice = art
+            
     return BestChoice
